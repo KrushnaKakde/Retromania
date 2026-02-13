80e4257b6ef2475a3ada04d3184a04e31bbf7cd8
KrushnaKakde <krushnakakde113@gmail.com>
2026-02-13 20:15:50 +0530

    Add owner model and creation route, fix routes paths, CSP header, debug setup

--- routes/ownersRouters.js
+++ routes/ownersRouters.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const ownerModel = require('../models/owner-model');
 const router = express.Router();
 
+if(process.env.NODE_ENV === "development"){
+    router.post("/create", async (req, res) => {
+        let owners = await ownerModel.find();
+        if(owners.length > 0){
+            return res
+            .status(400)
+            .send("Owner already exists");
+        }
 
+        let {fullname,email,password} = req.body;
+
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password,
+        });
+        res.status(201).send(createdOwner);
+    });
+}
 
 router.get("/", (req, res) => {
     res.send("Owner Route");
 });
+
 module.exports = router;
